b72166c8e1b7f705cc8efac251ba6209a222a8a2
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-03-22 18:46:34 -0700

    changes to configuration

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# D setting up my client cofig file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '	 PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identify file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '	  IdentifyFile ~/.ssh/school',
+  replace => true,

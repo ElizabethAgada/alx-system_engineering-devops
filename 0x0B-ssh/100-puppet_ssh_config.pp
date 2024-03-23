b4e363eaef6a683a5a8e41cad2ee2bba6ab48ba6
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-03-22 18:49:23 -0700

    changes to configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
-# D setting up my client cofig file
+# Seting up my client config file
 include stdlib
 
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '	 PasswordAuthentication no',
+  line   => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'Declare identify file':
+file_line { 'Delare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '	  IdentifyFile ~/.ssh/school',
+  line   => '     IdentityFile ~/.ssh/school',
   replace => true,
 }

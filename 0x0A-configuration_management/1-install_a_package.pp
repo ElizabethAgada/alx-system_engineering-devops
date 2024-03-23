faa7895d04ad31aee6ebbfd50bab6c21d15369c6
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-03-22 17:33:45 -0700

    Using Puppet, install flask from pip3.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Installs puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => gem,
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }

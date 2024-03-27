e5f86c14143911dd98a0b4211aecc94dd86132bd
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-03-27 15:01:01 -0700

    Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# kill process killmenow
-
-exec { 'pkill':
-  command  => 'pkill killmenow',
-  provider => 'shell',
+#!/usr/bin/pup
+# Dis is 2 install a specific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }

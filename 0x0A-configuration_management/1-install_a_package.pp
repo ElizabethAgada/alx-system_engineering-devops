81b076e51fa9f72cb1678506a466644b432e5469
Elizabeth Agada <lizzyegbo2015@gmail.com>
2024-03-27 17:52:04 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,10 @@
-#!/usr/bin/pup
-# Install a specific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# using puppet to install flask
+package { 'Flask':
+        ensure   => '2.1.0',
+        provider => 'pip3',
+}
+package { 'Werkzeug':
+        ensure   => '2.1.1',
+        provider => 'pip3',
 }
 

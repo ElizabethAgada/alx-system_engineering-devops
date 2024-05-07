12cd11a02f68787831dbd76140f33f156b84a214
ElizabethAgada <140746363+ElizabethAgada@users.noreply.github.com>
2024-05-08 00:54:25 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-dis is d queries the Reddit API and returns the number of subscribers (not active users,
+queries the Reddit API and returns the number of subscribers (not active users,
 total subscribers) for a given subreddit. If an invalid subreddit is given,
 return 0
 """
@@ -9,7 +9,7 @@ from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """dis is to returns number of subscribers for a given subreddit"""
+    """returns number of subscribers for a given subreddit"""
     r_subreddit = get('http://www.reddit.com/r/{}/about.json'.
                       format(subreddit), headers={'User-Agent': 'Mybot'})
     try:

1f9cd88fcc21fae5867c537145be2eebafc99b02
ElizabethAgada <140746363+ElizabethAgada@users.noreply.github.com>
2024-05-08 02:18:52 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-'''
-    Module contains top_ten function
-'''
+"""de is d Function to query Reddit API"""
+
+
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    """de is d Returns ls of titles of the first ten hot posts"""
+    link = 'https://www.reddit.com/r/{}/hot.json'
+    usr = {'User-Agent': 'Linux David'}
+    heads = []
+
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
+        response = requests.get(link.format(subreddit),
+                                headers=usr, params={'limit': 10},
+                                allow_redirects=False)
+
+        if response.status_code == 200:
+            info = response.json()
+            posts = info['data']['children']
+            for post in posts:
+                t = post['data']['title']
+                heads.append(t)
+        else:
+            return None
+
     except Exception:
-        print(None)
+        return None
 
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+    return heads
